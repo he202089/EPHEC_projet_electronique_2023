b2cbacb901d0d45f25fbe76da405481501183475
Amaury <iamaury1380@gmail.com>
2023-02-08 16:54:43 +0100

    Update main.py

--- pico/main.py
+++ pico/main.py
@@ -1,15 +1,12 @@
-# Intitiation du programme
+from io import *
+
 def init():
     pass
 
-# Boucle du programme
 def main_loop():
     pass
 
-# Interruptions
-pass
 
-# Lancement du programme
 init()
 while 1:
-    main_loop()
+    main_loop()    
