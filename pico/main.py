20a7e3a422445c0dcac839aa4c70491a2213d364
Amaury <iamaury1380@gmail.com>
2023-02-08 16:41:50 +0100

    Create main.py

---
+++ pico/main.py
@@ -0,0 +1,15 @@
+# Intitiation du programme
+def init():
+    pass
+
+# Boucle du programme
+def main_loop():
+    pass
+
+# Interruptions
+pass
+
+# Lancement du programme
+init()
+while 1:
+    main_loop()
